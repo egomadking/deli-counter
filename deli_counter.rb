d7e5145b8de9b58f9fa79179ce8e50777dd2a16b
Roy Mosby <ego.madking@gmail.com>
2020-12-09 10:34:07 -0500

    commit tree 45055ede9ba8d3f245c9023c78d73fbb8543da90

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def line(array)
+  if array.length() == 0
+    puts "The line is currently empty."
+  else
+    line = ""
+    array.each do |person|
+      line << " #{array.index(person) + 1}. #{person}"
+    end
+    puts "The line is currently:#{line}"
+  end
+end
+
+def take_a_number(array, name)
+  puts "Welcome, #{name}. You are number #{array.length + 1} in line."
+  array << name
+end
+
+def now_serving(array)
+  if array.length() == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
